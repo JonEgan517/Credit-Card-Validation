55565ed6d42245600f4c6f7274ee097e0cd99f12
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-11-15 11:56:26 -0700

    commit

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -6,15 +6,48 @@ public class CreditCard
 	public static void main (String[] args) throws IOException
 	{	        
 		Scanner file = new Scanner(new File("Credit Cards Numbers.txt")); 
-		int fileLength = file.nextInt();
-	  
-		for (int n = 0; n < fileLength; n++ )
-	    {   
-	    	 String line = file.nextLine();
-	         String [] readCard = line.split("");
-	         int size = readCard.length;
-	         
-	         System.out.println(line);
-	    }    
+		Scanner stringInput = new Scanner(System.in);
+		System.out.println("how many credit cards?");
+		int fileLength = stringInput.nextInt();
+		
+		for(int n = 0; n < fileLength; n++)
+		{
+	    	String line = file.nextLine(); 
+	        int[] readCard = new int[line.length()];
+	 		for (int i = 0; i < line.length(); i++) 
+	 		{
+	 			readCard[i] = Integer.parseInt(line.substring(i, i + 1));
+	 		}
+	 		
+	 		for (int i = readCard.length - 2; i >= 0; i = i - 2) 
+	 		{
+	 			int doubleIt = readCard[i];
+	 			
+	 			doubleIt = doubleIt * 2;
+	 			
+	 			if (doubleIt > 9) 
+	 			{
+	 				doubleIt = doubleIt % 10 + 1;
+	 			}
+	 			readCard[i] = doubleIt;
+	 		}
+	 		
+	 		int sum = 0;
+	 		
+	 		for (int i = 0; i < readCard.length; i++) 
+	 		{
+	 			sum += readCard[i];
+	 		}
+	 		
+	 		if (sum % 10 == 0) 
+	 		{
+	 			System.out.println(line + " is a valid credit card number");
+	 		} 
+	 		
+	 		else 
+	 		{
+	 			System.out.println(line + " is an invalid credit card number");
+	 		}
+		}    
 	}
 }
